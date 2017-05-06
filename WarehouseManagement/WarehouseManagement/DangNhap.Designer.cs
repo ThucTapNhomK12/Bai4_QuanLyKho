ed7a477cafc78d191eac6f77407f19395073129c
NguyenThiHao <nguyenhaomta@gmail.com>
2017-05-06 10:20:53 +0700

    Dang nhap

---
+++ WarehouseManagement/WarehouseManagement/DangNhap.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WarehouseManagement
+{
+    partial class DangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTaiKhoan = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.txtMatKhau = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.lblDangNhap = new DevComponents.DotNetBar.LabelX();
+            this.btnDangNhap = new DevComponents.DotNetBar.ButtonX();
+            this.lblMatKhau = new DevComponents.DotNetBar.LabelX();
+            this.lblTaiKhoan = new DevComponents.DotNetBar.LabelX();
+            this.SuspendLayout();
+            // 
+            // txtTaiKhoan
+            // 
+            // 
+            // 
+            // 
+            this.txtTaiKhoan.Border.Class = "TextBoxBorder";
+            this.txtTaiKhoan.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtTaiKhoan.Location = new System.Drawing.Point(127, 84);
+            this.txtTaiKhoan.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTaiKhoan.Name = "txtTaiKhoan";
+            this.txtTaiKhoan.PreventEnterBeep = true;
+            this.txtTaiKhoan.Size = new System.Drawing.Size(224, 22);
+            this.txtTaiKhoan.TabIndex = 6;
+            this.txtTaiKhoan.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtTaiKhoan_KeyDown);
+            // 
+            // txtMatKhau
+            // 
+            // 
+            // 
+            // 
+            this.txtMatKhau.Border.Class = "TextBoxBorder";
+            this.txtMatKhau.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtMatKhau.Location = new System.Drawing.Point(127, 129);
+            this.txtMatKhau.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.PasswordChar = '*';
+            this.txtMatKhau.PreventEnterBeep = true;
+            this.txtMatKhau.Size = new System.Drawing.Size(224, 22);
+            this.txtMatKhau.TabIndex = 8;
+            this.txtMatKhau.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtMatKhau_KeyDown);
+            // 
+            // lblDangNhap
+            // 
+            // 
+            // 
+            // 
+            this.lblDangNhap.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.lblDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDangNhap.ForeColor = System.Drawing.Color.Red;
+            this.lblDangNhap.Location = new System.Drawing.Point(125, 15);
+            this.lblDangNhap.Margin = new System.Windows.Forms.Padding(4);
+            this.lblDangNhap.Name = "lblDangNhap";
+            this.lblDangNhap.Size = new System.Drawing.Size(134, 43);
+            this.lblDangNhap.TabIndex = 11;
+            this.lblDangNhap.Text = "Đăng nhập";
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnDangNhap.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangNhap.Location = new System.Drawing.Point(127, 169);
+            this.btnDangNhap.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(113, 39);
+            this.btnDangNhap.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnDangNhap.TabIndex = 9;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
+            // 
+            // lblMatKhau
+            // 
+            // 
+            // 
+            // 
+            this.lblMatKhau.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.lblMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMatKhau.Location = new System.Drawing.Point(40, 129);
+            this.lblMatKhau.Margin = new System.Windows.Forms.Padding(4);
+            this.lblMatKhau.Name = "lblMatKhau";
+            this.lblMatKhau.Size = new System.Drawing.Size(79, 25);
+            this.lblMatKhau.TabIndex = 10;
+            this.lblMatKhau.Text = "Mật khẩu";
+            // 
+            // lblTaiKhoan
+            // 
+            // 
+            // 
+            // 
+            this.lblTaiKhoan.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.lblTaiKhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTaiKhoan.Location = new System.Drawing.Point(40, 84);
+            this.lblTaiKhoan.Margin = new System.Windows.Forms.Padding(4);
+            this.lblTaiKhoan.Name = "lblTaiKhoan";
+            this.lblTaiKhoan.Size = new System.Drawing.Size(79, 25);
+            this.lblTaiKhoan.TabIndex = 7;
+            this.lblTaiKhoan.Text = "Tài khoản";
+            // 
+            // DangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 240);
+            this.Controls.Add(this.txtTaiKhoan);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.lblDangNhap);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.lblMatKhau);
+            this.Controls.Add(this.lblTaiKhoan);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "DangNhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.Controls.TextBoxX txtTaiKhoan;
+        private DevComponents.DotNetBar.Controls.TextBoxX txtMatKhau;
+        private DevComponents.DotNetBar.LabelX lblDangNhap;
+        private DevComponents.DotNetBar.ButtonX btnDangNhap;
+        private DevComponents.DotNetBar.LabelX lblMatKhau;
+        private DevComponents.DotNetBar.LabelX lblTaiKhoan;
+    }
+}
+
---
+++ WarehouseManagement/WarehouseManagement/DangNhap.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WarehouseManagement
+{
+    public partial class DangNhap : Form
+    {
+        db_warehouse_managementEntities db = new db_warehouse_managementEntities();
+
+        public static tai_khoan USER_LOGIN = null;
+
+        public DangNhap()
+        {
+            InitializeComponent();
+        }
+
+        public void login()
+        {
+            if (txtTaiKhoan.Text.Length <= 0 || txtMatKhau.Text.Length <= 0)
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!", "Thông báo", MessageBoxButtons.OK);
+                return;
+            }
+            string username = txtTaiKhoan.Text;
+            string password = txtMatKhau.Text;
+            USER_LOGIN = db.tai_khoan.SingleOrDefault(x => x.tai_khoan1.Equals(username) &&
+                x.mat_khau.Equals(password));
+            if (USER_LOGIN == null)
+            {
+                MessageBox.Show("Sai tên đăng nhập hoặc mật khẩu!", "Thông báo", MessageBoxButtons.OK);
+                return;
+            }
+            else
+            {
+                this.Hide();
+                QuanLy form = new QuanLy();
+                form.Show();
+            }
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            login();
+        }
+
+        private void txtTaiKhoan_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                login();
+            }
+        }
+
+        private void txtMatKhau_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                login();
+            }
+        }
+    }
+}
